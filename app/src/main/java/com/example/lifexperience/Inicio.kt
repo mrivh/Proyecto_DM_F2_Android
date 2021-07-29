a057dcf977cadbc04ffee5e5049bd2379c26e9e9
RandyVillanueva1 <86336356+RandyVillanueva1@users.noreply.github.com>
2021-07-29 16:23:52 -0500

    Update Inicio.kt

--- app/src/main/java/com/example/lifexperience/Inicio.kt
+++ app/src/main/java/com/example/lifexperience/Inicio.kt
@@ -7,5 +7,10 @@ class Inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+        
+                imageView.setOnClickListener {
+
+            startActivity(Intent(this, Lugar::class.java))
+        }
     }
 }
