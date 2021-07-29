cc83f5b22a502912d3309e8c2ca393875d4e31f0
AcesTerra <jovengf@gmail.com>
2021-07-29 18:07:54 -0500

    Adding missing imports
    Signed-off-by: AcesTerra <jovengf@gmail.com>

--- app/src/main/java/com/example/lifexperience/Inicio.kt
+++ app/src/main/java/com/example/lifexperience/Inicio.kt
@@ -1,7 +1,9 @@
 package com.example.lifexperience
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
