8ca2278ff2be31a804740038b8dcdf75ef665559
RandyVillanueva1 <86336356+RandyVillanueva1@users.noreply.github.com>
2021-07-29 17:26:02 -0500

    Update Inicio.kt

--- app/src/main/java/com/example/lifexperience/Inicio.kt
+++ app/src/main/java/com/example/lifexperience/Inicio.kt
@@ -8,9 +8,34 @@ class Inicio : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
         
+        var imageView = findViewById<ImageButton>(R.id.imageView)
+
         imageView.setOnClickListener {
 
             startActivity(Intent(this, Lugar::class.java))
+    }
+
+        var imageView2 = findViewById<ImageButton>(R.id.imageView2)
+
+        imageView2.setOnClickListener {
+
+            startActivity(Intent(this, Inicio::class.java))
         }
+
+
+        var imageView4 = findViewById<ImageButton>(R.id.imageView4)
+
+        imageView4.setOnClickListener {
+
+            startActivity(Intent(this, Perfil_guia::class.java))
+        }
+        
+        var imageView5 = findViewById<ImageButton>(R.id.imageView5)
+
+        imageView5.setOnClickListener {
+
+            startActivity(Intent(this, Perfil_Usuario::class.java))
+        }
+
     }
 }
